81682fda30d168be94bf045e5de2fe58b0d80713
StrubleI <ivanjimenezblass@gmail.com>
2024-11-13 18:09:00 +0100

    Tienda Hecha
    hay que quitar la ui de fondo que ya esta hecha y poner el arco

--- Assets/Scripts/_Menus/Shop.cs
+++ Assets/Scripts/_Menus/Shop.cs
@@ -1,10 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class Shop : MonoBehaviour
 {
+    public TextMeshProUGUI coinDisplay; // TextMeshPro for displaying coins
+    public Button weapon1Button, weapon2Button, weapon3Button;
+    public int weapon1Cost = 100, weapon2Cost = 300, weapon3Cost = 250;
+
+    void Start()
+    {
+        UpdateCoinDisplay();
+
+        // Check purchase status and update button states
+        UpdateButtonStates();
+
+        // Assign button click events
+        weapon1Button.onClick.AddListener(() => BuyWeapon(1, weapon1Cost));
+        weapon2Button.onClick.AddListener(() => BuyWeapon(2, weapon2Cost));
+        weapon3Button.onClick.AddListener(() => BuyWeapon(3, weapon3Cost));
+    }
+
+    private void UpdateCoinDisplay()
+    {
+        int totalCoins = PlayerPrefs.GetInt("TotalCoins", 0);
+        coinDisplay.text = "Coins: " + totalCoins.ToString();
+    }
+
+    private void BuyWeapon(int weaponID, int cost)
+    {
+        int totalCoins = PlayerPrefs.GetInt("TotalCoins", 0);
+
+        if (totalCoins >= cost)
+        {
+            totalCoins -= cost;
+            PlayerPrefs.SetInt("TotalCoins", totalCoins); // Save the updated coin count
+            PlayerPrefs.SetInt("Weapon" + weaponID + "Purchased", 1); // Mark this weapon as purchased
+
+            UpdateCoinDisplay();
+            UpdateButtonStates(); // Update button states after purchase
+            Debug.Log("Weapon " + weaponID + " purchased!");
+        }
+        else
+        {
+            Debug.Log("Not enough coins to purchase Weapon " + weaponID);
+        }
+    }
+
+    private void UpdateButtonStates()
+    {
+        // Disable buttons if weapons are already purchased
+        if (PlayerPrefs.GetInt("Weapon1Purchased", 0) == 1)
+        {
+            weapon1Button.interactable = false;
+            weapon1Button.GetComponentInChildren<TextMeshProUGUI>().text = "Bought";
+        }
+        if (PlayerPrefs.GetInt("Weapon2Purchased", 0) == 1)
+        {
+            weapon2Button.interactable = false;
+            weapon2Button.GetComponentInChildren<TextMeshProUGUI>().text = "Bought";
+        }
+        if (PlayerPrefs.GetInt("Weapon3Purchased", 0) == 1)
+        {
+            weapon3Button.interactable = false;
+            weapon3Button.GetComponentInChildren<TextMeshProUGUI>().text = "Bought";
+        }
+    }
+
     public void Back()
     {
         SfxScript.TriggerSfx("SfxButton1");
