643c02461ca41fe7a9ba0ef39475148acc45837d
SergioZepol <s.lopezl.2021@alumnos.urjc.es>
2024-11-18 16:17:41 +0100

    Update CoinDisplayUI.cs

--- Assets/CoinDisplayUI.cs
+++ Assets/CoinDisplayUI.cs
@@ -1,18 +1,39 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CoinDisplayUI : MonoBehaviour
 {
     public TextMeshProUGUI coinDisplay;
-
+    private int totalCoins;
     void Start()
     {
+        totalCoins = 0;
         UpdateCoinDisplay();
     }
 
     void UpdateCoinDisplay()
     {
-        int totalCoins = PlayerPrefs.GetInt("TotalCoins", 0);
+        totalCoins = totalCoins + 10;
         coinDisplay.text = "Coins: " + totalCoins.ToString();
+    }
+
+    void OnEnable()
+    {
+        // Subscribirse al evento de cambio de escena
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        // Desubscribirse del evento de cambio de escena
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    // Método que se llama cuando una nueva escena es cargada
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Llama al método para cambiar la música cuando la escena cambia
+        UpdateCoinDisplay();
     }
 }
